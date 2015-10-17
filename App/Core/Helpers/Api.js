31779d9c1bcb5cb798c73ff75ec603ae4a76e5a4
Sam Granger <sam@weprovide.com>
2015-10-17 21:43:13 +0200

    Various changes

---
+++ App/Core/Helpers/Api.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var crypto = require('crypto');
+
+var Api = function() {
+  this._apiBaseUrl = 'https://society.com/api/v1';
+  options = options || function(){};
+};
+
+Api.prototype.request = function(options, data) {
+  return new Promise(function(resolve, fail) {
+  });
+};
